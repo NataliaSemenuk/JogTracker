039f2809771aa8e33dd854ca7b545d188ac05c00
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-12-12 13:13:12 +0300

    Has been added style for adaptive layout in component of authorization and replaced PNG images with SVG images

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import bearFaceImage from '../../images/bear-face.svg'
+import React, {Component} from 'react';
+import bearFaceWhiteImage from '../../images/bearFaceWhite/bear-face.png';
+import bearFacePurpleImage from '../../images/bearFacePurple/bearFace.png';
 import './Authorization.css';
 
-const Authorization = () => {
-    return (
-        <div className = 'authorizationBox'>
-            <div className='authorizationBox__img'>
-                <img src={bearFaceImage} className=''/>
+class Authorization extends Component {
+    state = {
+        bearImage: bearFaceWhiteImage,
+    }
+    updateDimensions() {
+        if(window.innerWidth < 480) {
+          this.setState({ bearImage:  bearFacePurpleImage});
+        } else {
+            this.setState({ bearImage:  bearFaceWhiteImage});
+        }
+    };
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+    };
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions.bind(this));
+    };
+    render() {
+        const {bearImage} = this.state;
+        return (
+            <div className = 'authorizationBox'>
+                <div className='authorizationBox__img'>
+                    <img src={bearImage}/>
+                </div>
+                <input type='button' value='Let me in' className='authorizationBox__btn'></input>
             </div>
-            <input type='button' value='Let me in' className='authorizationBox__btn'></input>
-        </div>
-    );
+        );
+    }
 }
 export default Authorization;
