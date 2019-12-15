28243a0bd72ae9c312340d3e674dbcda097f892d
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-12-15 21:25:33 +0300

    Has been created functional for routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from '../../pages/HomePage/HomePage';
+import JogsPage from '../../pages/JogsPage/JogsPage';
+import InfoPage from '../../pages/InfoPage/InfoPage';
+import AuthenticationPage from '../../pages/AuthenticationPage/AuthenticationPage';
 
 import './App.css';
 
 const App = () => {
     return (
         <div className = 'homePage'>
-            <HomePage/>
+            <Switch>
+                <Route exact path = '/' component={HomePage}/>
+                <Route  path = '/jogs' component={JogsPage}/>
+                <Route  path = '/info' component={InfoPage}/>
+                <Route  path = '/info' component={InfoPage}/>
+                <Route  path = '/authentication' component={AuthenticationPage}/>
+            </Switch>        
         </div>
     );
 }
