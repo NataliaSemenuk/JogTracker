37553aaa2f8f470d4ee8981d2a09ac2d2d312b8e
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-12-16 18:08:50 +0300

    Bug with date input has been fixed

--- src/pages/AuthenticationPage/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage/AuthenticationPage.jsx
@@ -1,45 +1,120 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import {Redirect, Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import cancelIcon from '../../images/cancel.svg';
 import './AuthenticationPage.css';
+import ApiService from '../../apiService/ApiService';
+import {addJog} from '../../actions/actions';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {Redirect} from 'react-router-dom';
+import DatePicker from 'react-date-picker';
 
 class AuthenticationPage extends Component {
-    onRedirect = () => {
-        console.log('1235');
-        return <Link to='/jogs' />
+    state = {
+        date: new Date(),
+        distance: null,
+        time: null,
+        isRedirect: false,
+    }
+    createNewJog = () => {
+        const {date, distance, time} = this.state;
+        const {jogs} = this.props;
+        return {
+            date: date,
+            distance: distance,
+            time: time,
+        }
+    }
+     onCreateJog = async () => {
+        const {date, distance, time} = this.state;
+        if (date && distance && time) {
+            const newJog = this.createNewJog();
+            console.log('NEW', newJog);
+            const apiService = new ApiService();
+            const token = JSON.parse(localStorage.getItem('token'));
+            try {
+                const response = await apiService.postJog(token, newJog);
+                console.log('newJog', response);
+            } catch (error) {
+                console.error(error);
+            }
+            this.setState({
+                isRedirect: true,
+            });
+        } else {
+            alert('Please, enter data');
+        }
+         
+    }
+    onChangeDistance = (event) => {
+        let valueDistance = event.target.value;
+        this.setState({
+            distance: valueDistance,
+        });
+    }
+    onChangeTime = (event) => {
+        let valueTime = event.target.value;
+        this.setState({
+            time: valueTime,
+        });
+    }
+    onChangeDate = (fullDate) => {
+        this.setState({
+            date: fullDate,
+        });
     }
     render() {
-        const {onClose} = this.props;
-        const distance = 'distance';
-        const time = 'time';
-        const data = 'data';
-        
-        
-        return (
-            <div className='authenticationPage'>
-                <Header/>
-                <div className = 'authentication'>
-                    <div className='authentication__btnClose'>
-                    <Link to='/jogs'><img src={cancelIcon}/></Link>
+        const {isRedirect, date} = this.state;
+        const distanceTitle = 'distance';
+        const timeTitle = 'time';
+        const dateTitle = 'date';      
+        if (isRedirect) {
+            return <Redirect to='/jogs' />
+        } else {
+            return (
+                <div className='authenticationPage'>
+                    <Header isHidenFilterIcon={true}/>
+                    <div className = 'authentication'>
+                        <div className='authentication__btnClose'>
+                        <Link to='/jogs'><img src={cancelIcon} alt='Close'/></Link>
+                        </div>
+                        <div className='authenticationBlock'>
+                            <label className='authentication__label'>{distanceTitle}</label>
+                            <input className='authentication__input' onChange={this.onChangeDistance}/>
+                        </div>
+                        <div className='authenticationBlock'>
+                            <time className='authentication__label'>{timeTitle}</time>
+                            <input className='authentication__input' onChange={this.onChangeTime}/>
+                        </div>
+                        <div className='authenticationBlock'>
+                            <data className='authentication__label'>{dateTitle}</data>
+                            <DatePicker 
+                                onChange={this.onChangeDate}
+                                value={date}
+                                calendarIcon={null}
+                                clearIcon={null}
+                                dayPlaceholder=''
+                                monthPlaceholder=''
+                                yearPlaceholder=''
+                                className='authentication__input'
+                            />
+                        </div>
+                        <input className='authentication__btnSave' type='button' value='save' onClick={this.onCreateJog}></input>
                     </div>
-                    <div className='authenticationBlock'>
-                        <label className='authentication__label'>{distance}</label>
-                        <input className='authentication__input'/>
-                    </div>
-                    <div className='authenticationBlock'>
-                        <time className='authentication__label'>{time}</time>
-                        <input className='authentication__input'/>
-                    </div>
-                    <div className='authenticationBlock'>
-                        <data className='authentication__label'>{data}</data>
-                        <input className='authentication__input'/>
-                    </div>
-                    <input className='authentication__btnSave' type='button' value='save'></input>
                 </div>
-            </div>
-        );
+            );
+        }    
+    }    
+}
+const mapStateToProps = ({jogs}) => {
+    return {
+        jogs,
     }
 }
-export default AuthenticationPage;
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        addJog
+    }, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticationPage);
